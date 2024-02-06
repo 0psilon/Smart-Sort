02d6da044272f264adb0b416f8d3cfdd5991a05f
Denis Demenkov <denis.demenkovg@gmail.com>
2024-02-05 19:26:21 -0600

    Added texts, commens and improved the code

--- main.py
+++ main.py
@@ -9,20 +9,25 @@ from dotenv import load_dotenv
 from PIL import Image
 from telebot import types
 
-from utils import CLASSES, PATH_TO_MODEL, TRANSFORM
+from utils import CLASSES, HINTS, MESSAGE_TEXTS, PATH_TO_MODEL, TRANSFORM
 
 load_dotenv()
 
 tg_token = os.getenv('tg_token')
-bot = telebot.TeleBot(tg_token)
+bot = telebot.TeleBot(tg_token, num_threads=4)
 
+# menu buttons
 bot.set_my_commands([
+    types.BotCommand('/hint', 'Хочу подсказку по сортировке!'),
     types.BotCommand('/help', 'Инструкция'),
 ])
 
 
 @bot.message_handler(commands=['start', 'help'])
 def start(message):
+    """
+    Sends a start message
+    """
     bot.send_message(
         message.chat.id,
         text=start_text,
@@ -31,19 +36,49 @@ def start(message):
 
 
 @bot.message_handler(content_types=['photo'])
-def predict_class(message):  
+def predict_class(message):
+    """
+    Receives a photo, sends back a model prediction.
+    Sends a message, then edits the message with the prediction.
+    """
+    message_to_edit = bot.send_message(
+        message.chat.id,
+        text=random.choice(MESSAGE_TEXTS)
+    )
+    # downloading
     file_info = bot.get_file(message.photo[-1].file_id)
     dwn_file = bot.download_file(file_info.file_path)
+    # image preprocessing
     byte_obj = io.BytesIO(dwn_file)
     img = Image.open(byte_obj)
     img = np.array(img, dtype=np.float32)
     img = TRANSFORM(image=img)['image']
     img = np.transpose(img, (2, 0, 1))
+    # model inference
     ort_inputs = {'input': img[None, ...]}
     ort_outs = ort_session.run(None, ort_inputs)
+
+    bot.edit_message_text(
+        text=f'Предсказанный класс: <b>{CLASSES[ort_outs[0].argmax()]}</b>',
+        chat_id=message.chat.id,
+        message_id=message_to_edit.message_id,
+        parse_mode='html'
+    )
+
+
+@bot.message_handler(commands=['hint'])
+def hint(message):
+    """
+    Randomly chooses and sends a text
+    from the texts/hints directory
+    """
+    file_path = random.choice(HINTS)
+    with open(file_path, 'r') as f:
+        to_send = f.read()
+
     bot.send_message(
         message.chat.id,
-        text=f'Предсказанный класс: <b>{CLASSES[ort_outs[0].argmax()]}</b>',
+        text=to_send,
         parse_mode='html'
     )
 
@@ -51,7 +86,7 @@ def predict_class(message):
 @bot.message_handler(content_types=['text'])
 def get_user_text(message):
     """
-    Обработка текстовых сообщений
+    Arbitrary text message handling + an easter egg
     """
     if message.text in ['❤️', '❤️❤️', '❤️❤️❤️']:
         to_send = '❤️' * random.choice([1, 3])
--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
-import albumentations as A
+import glob
 
+import albumentations as A
 
 RESCALE_SIZE = 224
 PATH_TO_MODEL = "weights/SmartSort.onnx"
+HINTS = glob.glob('texts/hints/*.txt')
 
 TRANSFORM = A.Compose([
     A.LongestMaxSize(RESCALE_SIZE),
@@ -17,8 +19,18 @@ CLASSES = [
     'Пластик',
     'Аккумуляторы',
     'Стекло',
-    'Метал',
+    'Металл',
     'Стекло',
     'Бумага',
     'Органика'
+]
+
+MESSAGE_TEXTS = [
+    'Идет распознавание...',
+    'Одну минутку...',
+    'Секундочку...',
+    'Предсказание совсем близко...',
+    'Буквально один момент...',
+    'Почти-почти...',
+    'И...'
 ]
